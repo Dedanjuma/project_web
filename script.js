34eaeaaa40136c8edcdf64d0b1b277524f7f3d2b
dedan <dedanjuma74@gmail.com>
2018-05-21 16:17:50 +0300

    git commit -m 'document.getElementById'

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById('heading').innerHTML = ("Dedan");
+    document.getElementById("heading").style.color = "blue";
+    document.getElementById("h2").style.color = "blue";
+}
