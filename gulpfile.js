5380ee6c5d601abaff8937af096e578b944a5563
dedan <dedanjuma74@gmail.com>
2018-05-22 14:22:56 +0300

    add gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
-const gulp = require('gulp')
-gulp.task('copyHTML', function(){return gulp.src('*.html')
-.pipe(gulp.dest('/html')})
+const gulp = require('gulp');
+const sass= require('gulp-sass');
+gulp.task('copyHTML', function()
+{return gulp.src('*.html')
+.pipe(gulp.dest('/html')
+})
+
+gulp.task('copyCSS',function(){
+  return gulp.src('*.scss')
+  .pipe(sass().on('error',sass.logError))
+  .pipe(gulp.dest('project_web'))
+})
